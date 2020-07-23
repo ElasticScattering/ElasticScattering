dab0d9280c2e3b573b8d8e96951d06aa79c1b9e4
Stijn Hinlopen <f.a.hinlopen@gmail.com>
2020-07-23 20:08:28 +0200

    Command line arguments

--- main.cpp
+++ main.cpp
@@ -6,12 +6,27 @@
 #include <random>
 #include <limits>
 #include <vector>
+#include <unordered_map>
 
 #include "opencl_utils.h"
 
 typedef unsigned int uint;
 
 #define RUN_CPU_SIM
+
+enum class Mode {
+    LIFETIME,
+    STATS,
+    AVG_DISTANCE,
+    CONDUCTIVITY
+}; 
+
+typedef struct
+{
+    int num_iterations;
+    Mode mode;
+    bool show_info;
+} InitParameters;
 
 typedef struct
 {
@@ -27,11 +42,8 @@ typedef struct
     cl_mem impb;
 } OCLResources;
 
-cl_platform_id selected_platform;
-cl_device_id selected_device;
-
 cl_int clStatus;
-double *result;
+double *result; // @Temporary
 
 void GPUElasticScattering(OCLResources *p_ocl, size_t size)
 {
@@ -48,11 +60,28 @@ void GPUElasticScattering(OCLResources *p_ocl, size_t size)
     CL_ERR_FAIL_COND_MSG(clStatus != CL_SUCCESS, clStatus, "Failed to read back result.");
 }
 
-void ParseArgs(OCLResources* p_ocl, int argc, char** argv) {
-    if (argc != 2) {
-        std::cout << "Usage: ElasticScattering [lifetime | distance | stats | conductivity] [show | no-show]" << std::endl;
+void ParseArgs(OCLResources* p_ocl, int argc, char** argv, InitParameters *p_init) {
+    if (argc != 4) {
+        std::cout << "Usage: ElasticScattering [num iterations] [lifetime | distance | stats | conductivity] [show | no-show]" << std::endl;
         exit(0);
     }
+
+    p_init->num_iterations = atoi(argv[1]);
+
+    std::unordered_map<std::string, Mode> modes
+    {
+        {"lifetime",     Mode::LIFETIME},
+        {"distance",     Mode::AVG_DISTANCE},
+        {"stats",        Mode::STATS},
+        {"conductivity", Mode::CONDUCTIVITY}
+    };
+    std::string key;
+    key.assign(argv[2], strlen(argv[2]));
+    auto iterator = modes.find(key);
+    ERR_FAIL_COND_MSG(iterator == modes.end(), "Couldn't understand second command line argument.");
+    p_init->mode = iterator->second;
+    
+    p_init->show_info = strcmp(argv[3], "show");
 }
 
 void PrepareOpenCLKernels(OCLResources* p_ocl, size_t size, double *data)
@@ -89,14 +118,14 @@ int main(int argc, char* argv[])
     const char* source_file = "program.cl";
 
     uint startHeight = 32, startWidth = 32;
-    uint impurityCount = 100;
     uint particleCount = 10000; //100'000'000; // batch size
     uint impurityCount = 1000;
 
     LARGE_INTEGER beginClock, endClock, clockFrequency;
     QueryPerformanceFrequency(&clockFrequency);
 
-    //parse_args(ocl, argc, argv);
+    InitParameters init;
+    ParseArgs(&ocl, argc, argv, &init);
 
     std::cout << "\n\n-----------------------------------------------" << std::endl;
     std::cout << "Initial field size: " << startHeight << ", " << startWidth << std::endl;
