c11437b89f7c8a670487a6c6a5ac6a1704132f34
Stijn Hinlopen <f.a.hinlopen@gmail.com>
2022-06-01 00:29:09 +0200

    License

--- src/sim/cl/cl_macros.h
+++ src/sim/cl/cl_macros.h
@@ -1,3 +1,22 @@
+/* -------------------------------------------------------------------------
+	This code is part of ElasticScattering.
+
+	Copyright(C) 2022 Stijn Hinlopen
+
+	This program is free software : you can redistribute it and /or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program.If not, see < http://www.gnu.org/licenses/>.
+   ------------------------------------------------------------------------ */
+
 #define GET_BASE_INDEX(m_i, m_j) ((m_j * get_global_size(0) * ss->particles_per_position) + (m_i * ss->particles_per_position))
 
 #define GET_INDEX(m_i, m_j, m_v) ((m_j * get_global_size(0)*get_global_size(2)) + (m_i * get_global_size(2)) + m_v)
